ed3d73971c05203cc26c90f1c531aa1c70ded915
Milad <moorara@users.noreply.github.com>
2021-10-26 23:35:38 -0400

    Add Ruby linter for formulas (#19)

--- changelog.rb
+++ changelog.rb
@@ -1,14 +1,15 @@
+# The formula for installing changelog binary.
 class Changelog < Formula
-  desc "The Simple Changelog Generator"
-  license "ISC"
-  homepage "https://github.com/gardenbed/changelog"
-  url "https://github.com/gardenbed/changelog.git",
-    tag: "v0.1.0",
-    revision: "da88f46f4ff1aabb45e0bc557cf124ab73c0a5a2"
-  head "https://github.com/gardenbed/changelog.git",
-    branch: "main"
+  desc 'The Simple Changelog Generator'
+  license 'ISC'
+  homepage 'https://github.com/gardenbed/changelog'
+  url 'https://github.com/gardenbed/changelog.git',
+      tag: 'v0.1.0',
+      revision: 'da88f46f4ff1aabb45e0bc557cf124ab73c0a5a2'
+  head 'https://github.com/gardenbed/changelog.git',
+       branch: 'main'
 
-  depends_on "go" => :build
+  depends_on 'go' => :build
 
   def install
     commit = `git rev-parse --short HEAD`
@@ -19,7 +20,7 @@ class Changelog < Formula
     go_version = go_version.strip
     build_time = build_time.strip
 
-    metadata_package = "github.com/gardenbed/changelog/metadata"
+    metadata_package = 'github.com/gardenbed/changelog/metadata'
     version_flag = "-X \"#{metadata_package}.Version=#{version}\""
     commit_flag = "-X \"#{metadata_package}.Commit=#{commit}\""
     branch_flag = "-X \"#{metadata_package}.Branch=main\""
@@ -28,13 +29,13 @@ class Changelog < Formula
     build_time_flag = "-X \"#{metadata_package}.BuildTime=#{build_time}\""
     ldflags = "#{version_flag} #{commit_flag} #{branch_flag} #{go_version_flag} #{build_tool_flag} #{build_time_flag}"
 
-    system "go", "build", "-ldflags", ldflags, "./cmd/changelog"
+    system 'go', 'build', '-ldflags', ldflags, './cmd/changelog'
 
-    bin.install "changelog"
+    bin.install 'changelog'
     prefix.install_metafiles
   end
 
   test do
-    system "#{bin}/changelog", "-version"
+    system "#{bin}/changelog", '-version'
   end
 end
