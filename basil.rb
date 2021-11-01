bc3151e372833a1db91ef2680147ecaa1cc90df6
Milad Irannejad <moorara@users.noreply.github.com>
2021-11-01 08:07:22 +0000

    [AUTOMATED] Update Formulas

--- basil.rb
+++ basil.rb
@@ -4,8 +4,8 @@ class Basil < Formula
   license 'ISC'
   homepage 'https://github.com/gardenbed/basil-cli'
   url 'https://github.com/gardenbed/basil-cli.git',
-      tag: 'v0.1.2',
-      revision: 'c31bb62fa2f85d365a00759884935f70b567164d'
+      tag: 'v0.1.3',
+      revision: 'c01a2ad65af5c153565ec60ada1a7ac6a570517d'
   head 'https://github.com/gardenbed/basil-cli.git',
        branch: 'main'
 
