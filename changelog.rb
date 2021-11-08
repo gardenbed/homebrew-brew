c7f2a50922f517c7b99a70f4e06c5605147dac19
Milad Irannejad <moorara@users.noreply.github.com>
2021-11-08 08:07:07 +0000

    [AUTOMATED] Update Formulas

--- changelog.rb
+++ changelog.rb
@@ -4,8 +4,8 @@ class Changelog < Formula
   license 'ISC'
   homepage 'https://github.com/gardenbed/changelog'
   url 'https://github.com/gardenbed/changelog.git',
-      tag: 'v0.1.2',
-      revision: '48a82a6bfbc46a274e121094082bcc510dbcac03'
+      tag: 'v0.1.3',
+      revision: '19b285ead2e01e538c9d765e1f9ea2eea13bf5c2'
   head 'https://github.com/gardenbed/changelog.git',
        branch: 'main'
 
